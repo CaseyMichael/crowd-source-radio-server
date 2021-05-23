398bdc6d45c368ca7cb8961f6f4e9c830a665d2a
Casey <caseymichaelpeters@gmail.com>
2021-05-23 13:07:39 -0600

    init

---
+++ src/main/java/com/peters/crowdsourceradio/CrowdSourceRadioApplication.java
@@ -0,0 +1,13 @@
+package com.peters.crowdsourceradio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdSourceRadioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdSourceRadioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peters/crowdsourceradio/CrowdSourceRadioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peters.crowdsourceradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdSourceRadioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
