2bab776e556755a773a8dfe284b9b1aa95d318b6
Casey <caseymichaelpeters@gmail.com>
2021-05-29 22:58:57 -0600

    added tests

--- .scripts/start.sh
+++ .scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "--- starting application"
 ./mvnw clean spring-boot:run
--- src/main/java/com/peters/crowdsourceradio/controllers/HelloWorldController.java
+++ src/main/java/com/peters/crowdsourceradio/controllers/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HelloWorldController {
+public class HelloWorldController {
 
     @GetMapping("/healthcheck")
     String heathcheck() {
