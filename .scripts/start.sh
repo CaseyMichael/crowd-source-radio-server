cc349a3bc4a2f2780042f30d0d21efd71dc3cb63
Casey <caseymichaelpeters@gmail.com>
2021-05-29 23:05:24 -0600

    Feature/ch43/create crowd source radio server repo (#1)
    * add clubhouse pr action
    
    * added hello world controller with single endpoint
    
    * added tests

---
+++ .scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "--- starting application"
+./mvnw clean spring-boot:run
--- src/main/java/com/peters/crowdsourceradio/CrowdSourceRadioApplication.java
+++ src/main/java/com/peters/crowdsourceradio/CrowdSourceRadioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrowdSourceRadioApplication {
+public class CrowdsourceRadioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrowdSourceRadioApplication.class, args);
+		SpringApplication.run(CrowdsourceRadioApplication.class, args);
 	}
 
 }
